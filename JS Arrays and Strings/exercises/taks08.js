e67f54058c0148e7bd8fa4891241a51ffeab0396
AleksandarSpasov2 <147007999+AleksandarSpasov2@users.noreply.github.com>
2025-03-16 19:27:24 +0200

    add: added exercises for lecture 07 Functions

---
+++ JS Arrays and Strings/exercises/taks08.js
@@ -0,0 +1,7 @@
+function findWords(text) {
+    let pattern = /[A-Z][a-z]*/g;
+
+    let result = text.match(pattern);
+
+    console.log(result.join(", "));
+}
---
+++ JS Funcrions & Statements/lab02.js
@@ -0,0 +1,5 @@
+function solve(value, exponentioal) {
+    return Math.pow(value, exponentioal);
+}
+
+console.log(solve(2, 6));
